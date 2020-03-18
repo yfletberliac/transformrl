c267c7bda2de14ee5783f6e53d64df6847ee9ad9
yfletberliac <yannisfbe@gmail.com>
2020-03-18 16:44:37 +0100

    feat(all): clean

--- run.py
+++ run.py
@@ -17,25 +17,3 @@ for env_id in ["AntBulletEnv-v0"]:
                      avec_coef=1., vf_coef=0., tensorboard_log=log_dir)
         model.learn(total_timesteps=10000, tb_log_name="tb/AVEC-PPO")
         # model.learn(total_timesteps=1000000, tb_log_name="tb/PPO")
-
-
-        ######################## TRPO ###########################
-        log_dir = "./logs/%s/AVEC-TRPO_%s" % (env_id, seed)
-        # log_dir = "./logs/%s/TRPO_%s" % (env_id, seed)
-        os.makedirs(log_dir, exist_ok=True)
-        env = make_vec_env(env_id, 1, seed, monitor_dir=log_dir)
-        model = TRPO('MlpPolicy', env, verbose=1,
-                     avec_coef=1., vf_coef=0., tensorboard_log=log_dir)
-        model.learn(total_timesteps=10000, tb_log_name="tb/AVEC-TRPO")
-        # model.learn(total_timesteps=1000000, tb_log_name="tb/TRPO")
-
-
-        ######################### SAC #############################
-        log_dir = "./logs/%s/AVEC-SAC_%s" % (env_id, seed)
-        # log_dir = "./logs/%s/SAC_%s" % (env_id, seed)
-        os.makedirs(log_dir, exist_ok=True)
-        env = make_vec_env(env_id, 1, seed, monitor_dir=log_dir)
-        model = SAC('CustomSACPolicy', env, verbose=1,
-                    avec_coef=1., value_coef=0., tensorboard_log=log_dir)
-        model.learn(total_timesteps=10000, tb_log_name="tb/AVEC-SAC")
-        # model.learn(total_timesteps=1000000, tb_log_name="tb/SAC")
